c639f620c265feb2d78cc4989a44d335da5c7fe4
brahim <ibrahim.imestir@ulb.be>
2021-03-25 13:13:10 +0100

    Project creation

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,11 @@
 package main;
 
 public class Main {
+
+    /**
+     * Main function
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World !");
     }
